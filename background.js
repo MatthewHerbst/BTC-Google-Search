587e77f6f141da02e1a77a07a90b2d663ec484f9
Brandon Harmon <harmonbc@miamiohedu>
2014-04-06 00:25:11 -0400

    midnight candy

--- background.js
+++ background.js
@@ -30,9 +30,8 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
   });
 
 function fetchImage(){
-	var tempImg = new Image();
-	tempImg.src="http://bitcoincharts.com/charts/chart.png?width=940&m=bitstampUSD&SubmitButton=Draw&r=5&i=15-min&c=0&s=&e=&Prev=&Next=&t=M&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=0&ps=0&l=0&p=0&";
-	saveImage("success.png",tempImg);
+	var src="http://bitcoincharts.com/charts/chart.png?width=940&m=bitstampUSD&SubmitButton=Draw&r=5&i=15-min&c=0&s=&e=&Prev=&Next=&t=M&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=0&ps=0&l=0&p=0&";
+	saveImageURLs([{1d: src}]);
 }
 
 /*
