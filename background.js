dd02a3f4da1c291abf6dbf619b411b364c7041d4
Brandon Harmon <harmonbc@miamiohedu>
2014-04-06 01:03:12 -0400

    Added the missing link

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ var bkg = chrome.runtime.getBackgroundPage();
 //Core data
 var data = {
     baseURL: "http://bitcoincharts.com/charts/chart.png?",
-    options {
+    options: {
         "width": "940",
         "height": "",
         "m": "bitstampUSD",
